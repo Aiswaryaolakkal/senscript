51b37b999f6bba63a9f80ccbec80764b7b76c7d3
TCS project <aiswaryaplus@gmail.com>
2024-01-05 12:41:41 +0530

    SECOND COMMIT

--- app/src/main/java/com/example/moive/view/product/phone/PhoneFragment.kt
+++ app/src/main/java/com/example/moive/view/product/phone/PhoneFragment.kt
@@ -1,6 +1,9 @@
 package com.example.moive.view.product.phone
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.os.Handler
+import android.widget.SearchView
 import androidx.fragment.app.viewModels
 import com.example.moive.BR
 import com.example.moive.R
@@ -8,23 +11,31 @@ import com.example.moive.base.BaseFragment
 import com.example.moive.databinding.FragmentPhoneBinding
 import com.example.moive.view.model.Product
 import com.example.moive.view.product.phone.adapter.ImageAdapter
-import com.example.moive.view.product.phone.adapter.PhoneListAdapter
-import com.example.moive.view.product.phone.adapter.TendingAdapter
+import com.example.moive.view.product.phone.adapter.TrendAdapter
+import com.example.moive.view.product.phone.adapter.TrailerAdapter
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.Timer
+import java.util.TimerTask
+
 
 @AndroidEntryPoint
 class PhoneFragment : BaseFragment<FragmentPhoneBinding, PhoneViewModel>(),
-    PhoneListAdapter.ItemClickListener,TendingAdapter.ItemClickListener {
+    TrendAdapter.ItemClickListener,TrailerAdapter.ItemClickListener {
     private var _binding: FragmentPhoneBinding? = null
     private val vm: PhoneViewModel by viewModels()
     lateinit var viewPagerAdapter: ImageAdapter
-    private val adapter: PhoneListAdapter by lazy {
-        PhoneListAdapter(requireContext(),pdtList,this)
+    var currentPage:Int = 0;
+    lateinit var timer:Timer;
+    val  DELAY_MS:Long = 500;//delay in milliseconds before task is to be executed
+    val  PERIOD_MS :Long= 3000
+    private val trendAdapter: TrendAdapter by lazy {
+        TrendAdapter(requireContext(),pdtList,this)
     }
-    private val trendAdapter: TendingAdapter by lazy {
-        TendingAdapter(requireContext(),pdtList,this)
+    private val trailerAdapter: TrailerAdapter by lazy {
+        TrailerAdapter(requireContext(),pdtList,this)
     }
     var pdtList : ArrayList<Product> = ArrayList()
+    var pdtListCopy : ArrayList<Product> = ArrayList()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         vm.fetchAlbumList()
@@ -45,14 +56,37 @@ class PhoneFragment : BaseFragment<FragmentPhoneBinding, PhoneViewModel>(),
         // on below line we are setting
         // adapter to our view pager.
         binding.viewPager.adapter = viewPagerAdapter
-        binding.adapter1 = adapter
-        binding.adapter2 = trendAdapter
+        binding.adapter1 = trendAdapter
+      binding.rvTrailerList.adapter = trailerAdapter
+        rotateSlider()
+
+        binding.searchvw.setOnQueryTextListener(object : SearchView.OnQueryTextListener,
+            androidx.appcompat.widget.SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            @SuppressLint("NotifyDataSetChanged")
+            override fun onQueryTextChange(newText: String?): Boolean {
+                if (newText != null) {
+                    pdtList.clear()
+                    pdtList.addAll(pdtListCopy.filter {
+                        it.title.lowercase().contains(newText.lowercase())
+
+                    })
+                    trailerAdapter.notifyDataSetChanged()
+                }
+                return false
+            }
+
+        })
 
     }
 
     override fun setupObservers() {
         vm.productList.observeForever {
             pdtList.addAll(it)
+            pdtListCopy.addAll(it)
         }
 
     }
@@ -62,6 +96,24 @@ class PhoneFragment : BaseFragment<FragmentPhoneBinding, PhoneViewModel>(),
         _binding = null
     }
 
+    fun rotateSlider(){
+        val handler = Handler()
+        val Update = Runnable {
+            if (currentPage === 3 - 1) {
+                currentPage = 0
+            }
+            binding.viewPager.setCurrentItem(currentPage++, true)
+        }
+
+        timer = Timer() // This will create a new Thread
+
+        timer.schedule(object : TimerTask() {
+            // task to be scheduled
+            override fun run() {
+                handler.post(Update)
+            }
+        }, DELAY_MS, PERIOD_MS)
+    }
      override fun onItemclicked(item:Product) {
     }
 
--- app/src/main/java/com/example/moive/view/product/phone/adapter/TrendAdapter.kt
+++ app/src/main/java/com/example/moive/view/product/phone/adapter/TrendAdapter.kt
@@ -4,23 +4,23 @@ import android.content.Context
 import com.bumptech.glide.Glide
 import com.example.moive.R
 import com.example.moive.base.recycleview.BaseAdapter
-import com.example.moive.databinding.ItemTrailerBinding
+import com.example.moive.databinding.ItemPhoneListBinding
 import com.example.moive.view.model.Product
 
-class TendingAdapter (
-    val context: Context,
+class TrendAdapter(
+    val context:Context,
     var list: List<Product>,
     val callBack: ItemClickListener,
-) : BaseAdapter<ItemTrailerBinding, Product>(list) {
-    override val layoutId: Int = R.layout.item_trailer
+) : BaseAdapter<ItemPhoneListBinding, Product>(list) {
+    override val layoutId: Int = R.layout.item_phone_list
 
-    override fun bind(binbing: ItemTrailerBinding, item: Product, position: Int) {
+    override fun bind(binbing: ItemPhoneListBinding, item: Product, position: Int) {
         binbing.model = item
         Glide.with(context).load(item.thumbnail).into(binbing.ivMovie);
 
     }
 
     interface ItemClickListener {
-        fun onTrendItemclicked( item: Product)
+        fun onItemclicked( item: Product)
     }
 }
